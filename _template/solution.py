8a9746a6c4a093ab6e684672136c9fac394968d9
Athena Yao <athena@discordapp.com>
2022-12-03 00:34:59 -0800

    improve template

--- _template/solution.py
+++ _template/solution.py
@@ -1,12 +1,33 @@
-#! /Users/athena/.nix-profile/bin/python
+#! python3
+from optparse import OptionParser
+import sys
 
-def run(filename):
+def run_part_1(filename):
     with open(filename, 'r') as file:
         for raw_line in file.readlines():
             line = raw_line.strip()
             print(".." + line)
             # do something
 
+
+def run_part_2(filename):
+    with open(filename, 'r') as file:
+        for raw_line in file.readlines():
+            line = raw_line.strip()
+            print(".." + line)
+            # do something
+
+
 if __name__ == '__main__':
-    run('./sample-input.txt')
-    # run ('./input.txt')
+    mode = sys.argv[1]
+    if not mode:
+        mode == "1a"
+
+    if mode == "1a":
+        run_part_1('./sample-input.txt')
+    elif mode == "1b":
+        run_part_1("./input.txt")
+    elif mode == "2a":
+        run_part_2('./sample-input.txt')
+    else:
+        run_part_2('./input.txt')
