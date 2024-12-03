d614f8f59745f9b7be553afd95154d142ac02b66
Athena Yao <athena@qq.is>
2024-12-02 22:11:32 -0800

    not working day 3 part 2

--- 2024/03/b.py
+++ 2024/03/b.py
@@ -5,23 +5,31 @@ import os
 import re
 
 MUL_RE = re.compile(r'mul\((\d+),(\d+)\)')
+DO_DONT_RE = re.compile(r'(do\(\)|don\'t\(\))')
 
 def run(lines):
     sum = 0
     line = "".join(lines)
+
+    enabled_at = [0]
+    
+    dd_pos = 0
+    is_enabled = True
+    the_match = None
+    while match:= DO_DONT_RE.search(line, dd_pos):
+        for _ in range(dd_pos, match.start()):
+            enabled_at.append(is_enabled)
+        is_enabled = match.groups()[0] == 'do()'
+        dd_pos = match.start() + 1
+        the_match = match
+
+    for _ in range(the_match.start(), len(line)+1):
+        enabled_at.append(is_enabled)
+
     pos = 0
-    is_start = True
     while match := MUL_RE.search(line, pos):
         pos = match.start() + 1
-        is_enabled = False
-        if is_start:
-            is_enabled = True
-            is_start = False
-        else:
-            last_do = line[0:pos].rfind('do()')
-            last_dont = line[0:pos].rfind('don\'t()')
-            is_enabled = last_do > last_dont
-        if is_enabled:
+        if enabled_at[pos]:
             sum += int(match.groups()[0], 10) * int(match.groups()[1], 10)
 
     return sum
