e091ce13c34a6372d1e74151412bedae8533ba73
Athena Yao <athena@qq.is>
2024-12-06 21:37:35 -0800

    cleaned it up

--- 2024/07/b.py
+++ 2024/07/b.py
@@ -4,52 +4,23 @@ import sys
 import os
 
 
-def find_max(numbers):
-    max_possible = 1
-    for number in numbers:
-        max_possible *= number
-    return max_possible
-
-def find_min(numbers):
-    min_possible = 0
-    for number in numbers:
-        min_possible += number
-    return min_possible
-
-
 def mul(expected_result, total_so_far, numbers):
-    if not numbers:
-        return total_so_far == expected_result
-    
     total_so_far *= numbers[0]
-    if mul(expected_result, total_so_far, numbers[1:]):
-        return True
-    elif sum(expected_result, total_so_far, numbers[1:]):
-        return True
-    return concat(expected_result, total_so_far, numbers[1:])
+    return find_equations(expected_result, total_so_far, numbers[1:])
 
 def sum(expected_result, total_so_far, numbers):
-    if len(numbers) == 0:
-        return total_so_far == expected_result
-    
     total_so_far += numbers[0]
-    if sum(expected_result, total_so_far, numbers[1:]):
-        return True
-    elif mul(expected_result, total_so_far, numbers[1:]):
-        return True
-    return concat(expected_result, total_so_far, numbers[1:])
-
+    return find_equations(expected_result, total_so_far, numbers[1:])
 
 def concat(expected_result, total_so_far, numbers):
+    total_so_far = int(str(total_so_far) + str(numbers[0]))
+    return find_equations(expected_result, total_so_far, numbers[1:])
+
+def find_equations(expected_result, total_so_far, numbers):
     if len(numbers) == 0:
         return total_so_far == expected_result
     
-    total_so_far = int(str(total_so_far) + str(numbers[0]))
-    if sum(expected_result, total_so_far, numbers[1:]):
-        return True
-    elif mul(expected_result, total_so_far, numbers[1:]):
-        return True
-    return concat(expected_result, total_so_far, numbers[1:])
+    return sum(expected_result, total_so_far, numbers) or mul(expected_result, total_so_far, numbers) or concat(expected_result, total_so_far, numbers)
 
 def run(lines):
     valid_sum = 0
@@ -57,17 +28,8 @@ def run(lines):
         test_value, numbers_raw = line.split(":")
         test_value = int(test_value)
         numbers = [int(x) for x in numbers_raw.split()]
-        max_possible = find_max(numbers)
-        min_possible = find_min(numbers)
         
-        # if test_value < min_possible or test_value > max_possible:
-        #     continue
-        
-        # if test_value == min_possible or test_value == max_possible:
-        #     valid_sum += test_value
-        #     continue
-        
-        is_valid = sum(test_value, 0, numbers) or mul(test_value, 0, numbers) or concat(test_value, 0, numbers)
+        is_valid = find_equations(test_value, 0, numbers)
         if is_valid:
             valid_sum += test_value
     return valid_sum
